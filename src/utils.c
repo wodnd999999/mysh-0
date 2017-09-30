9c93252bf618036bba25905ab86b87f621b3eb8b
Jaeung Choi <wodnd999999@ajou.ac.kr>
2017-09-30 18:38:46 +0000

    Add parsing code

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,57 @@
 #include "utils.h"
+#include <malloc.h>
+
+#define MAX 8096
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	int state = 0;
+	int count = 0;
+	int start[MAX], end[MAX];
+	int i, j;
+
+	for(i = 0; command[i]; i++)
+	{
+		switch(state)
+		{
+		case 0:
+			if(!(command[i] == ' ' || command[i] == '\t' || command[i] == '\n'))
+			{
+				state = 1;
+				start[count] = i;
+			}
+			break;
+		case 1:
+			if(command[i] == ' ' || command[i] == '\t' || command[i] == '\n')
+			{
+				state = 0;
+				end[count] = i;
+				count++;
+			}
+			break;
+		}
+	}
+	if(state == 1)
+		end[count++] = i;
+
+	if(count)
+	{
+		(*argc) = count;
+		(*argv) = (char**)malloc(sizeof(char*) * count);
+		for(i = 0; i < count; i++)
+		{
+			(*argv)[i] = (char*)malloc(sizeof(char) * (end[i] - start[i] + 1));
+			for(j = start[i]; j < end[i]; j++)
+				(*argv)[i][j - start[i]] = command[j];
+			(*argv)[i][end[i] - start[i]] = '\0';
+		}
+	}
+	else
+	{
+		(*argc) = 1;
+		(*argv) = (char**)malloc(sizeof(char*));
+		(*argv)[0] = (char*)malloc(sizeof(char));
+		(*argv)[0][0] = '\0';
+	}
 }
